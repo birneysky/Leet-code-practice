94c8e6b4474bf44d0fce27b7e0294f9b0cb1b97a
birneysky <birneysky@163.com>
2016-03-04 16:34:43 +0800

    modify

---
+++ leet-code/ArraySolution1.cpp
@@ -0,0 +1,9 @@
+//
+//  ArraySolution1.cpp
+//  leet-code
+//
+//  Created by zhangguang on 16/3/4.
+//  Copyright © 2016年 com.github. All rights reserved.
+//
+
+#include "ArraySolution1.hpp"
---
+++ leet-code/ArraySolution1.hpp
@@ -0,0 +1,14 @@
+//
+//  ArraySolution1.hpp
+//  leet-code
+//
+//  Created by zhangguang on 16/3/4.
+//  Copyright © 2016年 com.github. All rights reserved.
+//
+
+#ifndef ArraySolution1_hpp
+#define ArraySolution1_hpp
+
+#include <stdio.h>
+
+#endif /* ArraySolution1_hpp */
