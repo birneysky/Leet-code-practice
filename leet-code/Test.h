8d52ca571812d8eb78e618745c3715dfe5e3c33f
yingwei0831 <birneysky@163.com>
2016-03-22 21:55:17 +0800

    modify

---
+++ leet-code/Test.h
@@ -0,0 +1,19 @@
+//
+//  Test.h
+//  leet-code
+//
+//  Created by birneysky on 16/3/16.
+//  Copyright © 2016年 com.github. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+/*
+ 
+ @property 后面可以有哪些修饰符
+ 
+ */
+
+@end
---
+++ leet-code/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  leet-code
+//
+//  Created by birneysky on 16/3/16.
+//  Copyright © 2016年 com.github. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
